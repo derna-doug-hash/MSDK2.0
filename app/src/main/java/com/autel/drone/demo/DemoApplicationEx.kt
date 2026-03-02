9d6e3c614343a1d7a021891acbcb318807e956a8
derna-doug-hash <douglaseesley@gmail.com>
2026-03-01 22:44:35 -0500

    Update DemoApplicationEx.kt
    Add SDK Key,"

--- app/src/main/java/com/autel/drone/demo/DemoApplicationEx.kt
+++ app/src/main/java/com/autel/drone/demo/DemoApplicationEx.kt
@@ -8,7 +8,7 @@ class DemoApplicationEx : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        SDKManager.get().init(applicationContext, true)
+        SDKManager.get().init(applicationContext, "97ffacc0-c832-4a0d-b008-6f8e9e8cbd37", true)
         SDKLogManager.get().setSDKLogPath(applicationContext)
         println("SDKManager V=${SDKManager.get().getSDKVersion()}")
     }
